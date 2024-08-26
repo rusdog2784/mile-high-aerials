d48691e2f8a2ea7d5f5688db7047ed1976ee07f1
Scott's Big Boy <srussell1383@gmail.com>
2024-08-26 10:29:59 -0600

    Adding some helpful 'research' links.

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,4 +14,12 @@ provider "aws" {
 # TODO: Left off looking into terraform, github actions, and terraform's vault
 # 		provider. My end goal is to get this service deployed to AWS using 
 #		terraform. Then any updates and changes to the service will be done 
-# 		through terraform via github actions and vault.
+# 		through terraform via github actions and vault. Oh, and here are some 
+#		helpful links I was using:
+#		- https://docs.github.com/en/actions/writing-workflows/quickstart
+#		- https://www.freecodecamp.org/news/how-to-deploy-aws-infrastructure-with-terraform-and-github-actions-a-practical-multi-environment-ci-cd-guide/
+#		- https://developer.hashicorp.com/terraform/tutorials/secrets/secrets-vault
+#		- https://developer.hashicorp.com/vault/tutorials/getting-started/getting-started-deploy#initialize-vault
+#		- https://developer.hashicorp.com/vault/docs/configuration
+#		- https://developer.hashicorp.com/vault/tutorials/app-integration/github-actions#start-vault
+#		- https://developer.hashicorp.com/terraform/tutorials/automation/github-actions#set-up-hcp-terraform
